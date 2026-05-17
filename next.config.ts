05828f07c196606b73fd39028f4f1a1c4ba5e321
charisredcomm <muhammad.charis@redcomm.co.id>
2026-05-17 14:38:50 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,16 @@ const nextConfig: NextConfig = {
         hostname: "lh3.googleusercontent.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+        pathname: "/storage/v1/object/public/resume/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.storage.supabase.co",
+        pathname: "/storage/v1/object/public/resume/**",
+      },
     ],
   },
 };
